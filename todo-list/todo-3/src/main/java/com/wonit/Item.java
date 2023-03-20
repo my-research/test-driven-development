fd9d2177a8639dd34301bacb5d86049bb753a0a9
dhslrl321 <dhslrl321@gmail.com>
2023-03-20 23:22:39 +0900

    commit

---
+++ todo-list/todo-3/src/main/java/com/wonit/Item.java
@@ -0,0 +1,5 @@
+package com.wonit;
+
+public class Item {
+
+}
--- todo-list/todo-3/src/main/java/com/wonit/Main.java
+++
@@ -1,7 +0,0 @@
-package com.wonit;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("args = " + args);
-    }
-}
