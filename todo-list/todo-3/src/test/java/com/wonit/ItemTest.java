65d9fda2ac54b899eb6848f5b32995b9803ef5c9
dhslrl321 <dhslrl321@gmail.com>
2023-03-24 21:55:15 +0900

    commit

--- todo-list/todo-3/src/test/java/com/wonit/ItemTest.java
+++ todo-list/todo-3/src/test/java/com/wonit/ItemTest.java
@@ -1,7 +1,15 @@
 package com.wonit;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ItemTest {
 
+    Item sut;
+
+    @Test
+    void name() {
+
+    }
 }
