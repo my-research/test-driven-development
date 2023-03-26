caded07899ed81ea755c2b5bd8e899b4bd9beb69
dhslrl321 <dhslrl321@gmail.com>
2023-03-26 23:15:19 +0900

    commit

--- todo-list/todo-3/src/main/java/com/wonit/Item.java
+++ todo-list/todo-3/src/main/java/com/wonit/Item.java
@@ -2,4 +2,7 @@ package com.wonit;
 
 public class Item {
 
+    public static Item newItem() {
+        return new Item();
+    }
 }
--- todo-list/todo-3/src/test/java/com/wonit/ItemTest.java
+++ todo-list/todo-3/src/test/java/com/wonit/ItemTest.java
@@ -1,15 +1,18 @@
 package com.wonit;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ItemTest {
 
     Item sut;
 
     @Test
-    void name() {
+    void can_create() {
+        Item actual = Item.newItem();
 
+        assertThat(actual).isNotNull();
     }
 }
